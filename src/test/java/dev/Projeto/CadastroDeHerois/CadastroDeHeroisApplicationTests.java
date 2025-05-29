77d002ecc6178388d920ec72964a32b94d0eae86
unknown <l.lilinaipe@hotmail.com>
2025-05-28 23:30:35 -0300

    CadastroDeNinja

---
+++ src/test/java/dev/Projeto/CadastroDeHerois/CadastroDeHeroisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Projeto.CadastroDeHerois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeHeroisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
